b3debf714391226e9bbaef253e021689536cb5d1
chzhrntmf1 <chzhrntmf@naver.com>
2021-12-19 22:58:50 +0900

    Initial commit

---
+++ src/main/java/com/example/MyFirstORM/MyFirstOrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyFirstORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MyFirstORM/MyFirstOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyFirstORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
